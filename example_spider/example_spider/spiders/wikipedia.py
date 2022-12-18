716d3eed4e08d9c5cfb4b7f1c38443aa4663e396
Uszczi <mateuszpapuga24@gmail.com>
2022-12-18 18:23:30 +0100

    fix domain

--- example_spider/example_spider/spiders/wikipedia.py
+++ example_spider/example_spider/spiders/wikipedia.py
@@ -2,9 +2,9 @@ import scrapy
 
 
 class WikipediaSpider(scrapy.Spider):
-    name = 'wikipedia'
-    allowed_domains = ['wikipedia.com']
-    start_urls = ['http://wikipedia.com/']
+    name = "wikipedia"
+    allowed_domains = ["wikipedia.org"]
+    start_urls = ["http://wikipedia.org/"]
 
     def parse(self, response):
         pass
