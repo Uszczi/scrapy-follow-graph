85573c67191e93f5d85572f76b4f2070cf55c58a
Uszczi <mateuszpapuga24@gmail.com>
2022-12-18 19:05:50 +0100

    wip example spider

--- example_spider/example_spider/spiders/wikipedia.py
+++ example_spider/example_spider/spiders/wikipedia.py
@@ -4,7 +4,20 @@ import scrapy
 class WikipediaSpider(scrapy.Spider):
     name = "wikipedia"
     allowed_domains = ["wikipedia.org"]
-    start_urls = ["http://wikipedia.org/"]
+    start_urls = ["https://pl.wikipedia.org/wiki/Bumerang"]
+
+    a = 0
 
     def parse(self, response):
-        pass
+        if self.a < 5:
+            self.a += 1
+            links = response.css(
+                ".mw-parser-output > *:not(#Vorlage_Alternative):not(.metadata) a[href^='/wiki']::attr(href)"
+            ).getall()
+            links = [link for link in links if "." not in link]
+            yield response.follow(links[0])
+        else:
+            yield {
+                "url": response.url,
+                "title": response.css(".mw-page-title-main::text").get(),
+            }
